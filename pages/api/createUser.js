fa1c192ac56c1941f0a209b0fba0b6028171a39e
Matt Heindel <mheindel94@gmail.com>
2021-08-30 15:30:20 -0700

    Fix API response bug using promises

--- pages/api/createUser.js
+++ pages/api/createUser.js
@@ -1,7 +1,12 @@
 import { createUser } from '../../lib/models'
 
 export default function handler(req, res) {
-  createUser(req.body)
-    .then(result => res.status(201).send(result))
+  return new Promise(resolve => {
+    createUser(req.body)
+    .then(result => {
+      res.status(201).send(result)
+      resolve()
+    })
     .catch(err => res.status(400).send(err))
+  })
 }
