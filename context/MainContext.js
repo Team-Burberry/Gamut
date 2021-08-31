2731e882a8e9e3443c6d999bea210c1eb74288a4
Amalia Bryant <amaliacbryant@gmail.com>
2021-08-30 22:30:24 -0700

    implementing next.js router

--- context/MainContext.js
+++ context/MainContext.js
@@ -1,6 +1,28 @@
-import {createContext} from 'react'
+import {createContext} from 'react';
+
 const MainContext = createContext({
+  user: null
+});
 
-})
+export default MainContext;
 
-export default MainContext
+/*
+Set an authentication state observer and get user data
+For each of your app's pages that need information about the signed-in user, attach an observer to the global authentication object. This observer gets called whenever the user's sign-in state changes.
+
+Attach the observer using the onAuthStateChanged method. When a user successfully signs in, you can get information about the user in the observer.
+*/
+// import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+// const auth = getAuth();
+// onAuthStateChanged(auth, (user) => {
+//   if (user) {
+//     // User is signed in, see docs for a list of available properties
+//     // https://firebase.google.com/docs/reference/js/firebase.User
+//     const uid = user.uid;
+//     // ...
+//   } else {
+//     // User is signed out
+//     // ...
+//   }
+// });
