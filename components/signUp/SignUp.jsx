4774861b4be062cc290795ccc0d6fc1188bbc2ee
Amalia Bryant <amaliacbryant@gmail.com>
2021-08-30 20:10:06 -0700

    added styling and breaking into modulars

--- components/signUp/SignUp.jsx
+++ components/signUp/SignUp.jsx
@@ -2,42 +2,55 @@ import React from 'react';
 import firebase from '../../firebase.js'
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
-import {Button} from "@chakra-ui/react"
+import {Heading, VStack, Input, Select, Button} from "@chakra-ui/react"
 
 const SignUp = () => (
   <div>
-    <h2>SignUp</h2>
+    <Heading>SignUp</Heading>
     <form>
-      <input type="text" placeholder="name"/>
-      <input type="email" placeholder="email@email.com"/>
-      <input type="password" placeholder="******"/>
-      <input type="date"/>
-      <input type="text" placeholder="city"/>
-      <input type="text" placeholder="state"/>
-      <select name="gender">
-        <option value="">Gender selection</option>
-        <option value="Male">Male</option>
-        <option value="Female">Female</option>
-        <option value="non-binary">Non Binary</option>
-      </select>
-      {/* <button>Next</button> */}
-      <Button colorScheme="orange" onClick={e=>{
-        e.preventDefault();
-        const email = 'teamBurberry@gmail.com';
-        const password = 'topSecretP455word';
+      <VStack>
+        <Input size="sm" type="text" placeholder="name"/>
+        <Input size="sm" type="email" placeholder="email@email.com"/>
+        <Input size="sm" type="password" placeholder="******"/>
+        <Input size="sm" type="date"/>
+        <Input size="sm" type="text" placeholder="city"/>
+        <Input size="sm" type="text" placeholder="state"/>
+        <Select name="gender" size="sm">
+          <option value="">Gender selection</option>
+          <option value="Male">Male</option>
+          <option value="Female">Female</option>
+          <option value="non-binary">Non Binary</option>
+        </Select>
+        {/* <button>Next</button> */}
+        <Button colorScheme="orange" onClick={e=>{
+          e.preventDefault();
+          //verify all the form data
+          //if account is created
+            //submit account info to db
+              //if post is successful
+                //forward user to feed page
+              //else
+                //notify user of error
+          //else
+            //notify user of error
 
-        const auth = getAuth();
-        createUserWithEmailAndPassword(auth, email, password)
-          .then((userCredential) => {
-            const user = userCredential.user;
-            console.log(userCredential);
-          })
-          .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.log(errorCode, errorMessage);
-          });
-      }}>Sign Up</Button>
+          const email = 'teamBurberry@gmail.com';
+          const password = 'topSecretP455word';
+
+          const auth = getAuth();
+          createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+              const user = userCredential.user;
+              console.log(userCredential);
+            })
+            .catch((error) => {
+              const errorCode = error.code;
+              const errorMessage = error.message;
+              console.log(errorCode, errorMessage);
+            });
+        }}>Sign Up</Button>
+      </VStack>
+
     </form>
     {/* <p>Have an account? <a href="./login">login</a></p> */}
   </div>
