c4116f326e82d974750caf7222a0786148483a5e
Amalia Bryant <amaliacbryant@gmail.com>
2021-09-01 17:27:11 -0700

    save

--- components/splash/Splash.jsx
+++ components/splash/Splash.jsx
@@ -12,12 +12,14 @@ import {Heading, VStack} from "@chakra-ui/react";
 
 const Splash = () => {
   useEffect(() => {
-    const auth = getAuth();
+    setTimeout(()=>{
+      const auth = getAuth();
     if (auth.currentUser) {
       Router.push('/search');
     } else {
       Router.push('/login');
     }
+  }, 5000);
   });
 
   return (
